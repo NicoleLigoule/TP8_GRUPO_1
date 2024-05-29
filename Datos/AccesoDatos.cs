1d174fd52f55c1f42f5a9655472afdf67706e3ba
BasiliscX <juniornavarro1988@gmail.com>
2024-05-29 13:26:14 -0300

    AccesoDatos.cs
    conexión y controlador.

---
+++ Datos/AccesoDatos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class AccesoDatos
+    {
+        private string rutaBDNeptuno = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True";
+
+        private SqlConnection ObtenerConexion()
+        {
+            SqlConnection cn = new SqlConnection(rutaBDNeptuno);
+            try
+            {
+                cn.Open();
+                return cn;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+        private SqlDataAdapter ObtenerAdaptador(String consultaSql, SqlConnection cn)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(consultaSql, cn);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
